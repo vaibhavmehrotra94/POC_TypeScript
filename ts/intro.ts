397b46c7da38cbafaa67ca56f68193602a11112c
vaibhavmehrotra1194 <vaibhavmehrotra1194@gmail.com>
2023-05-10 19:02:50 +0530

    Chore: add data type list & remove comments in compiled js

--- ts/intro.ts
+++ ts/intro.ts
@@ -1,3 +1,26 @@
 let user = { name: "vab", age: 10, email:"yes" };
 
 console.log(user.email);
+
+
+/*
+* Basic Types
+*
+* Number - 123
+* String - "Hello 123"
+* Boolean - true/false
+* Null - null
+* Undefined - undefined
+* Void - ?
+*
+* Data Structures
+* Object - {}
+* Array - []
+* Tuples - [] typed array with predefined length
+* ....
+*
+* Special Case Scenario
+* Any - any
+* Never - ?
+* Unknown - ?
+*/
