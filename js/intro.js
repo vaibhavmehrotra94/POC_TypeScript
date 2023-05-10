eb775e6b8499c40fa4634eb887f75e544a077540
vaibhavmehrotra1194 <vaibhavmehrotra1194@gmail.com>
2023-05-10 18:51:29 +0530

    chore: initial setup

---
+++ js/intro.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let user = { name: "vab", age: 10, email: "yes" };
+console.log(user.email);
---
+++ ts/intro.ts
@@ -0,0 +1,3 @@
+let user = { name: "vab", age: 10, email:"yes" };
+
+console.log(user.email);
