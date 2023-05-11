6609f7b94453ccbef2fdea577e541d52f1fc16d1
vaibhavmehrotra1194 <vaibhavmehrotra1194@gmail.com>
2023-05-12 00:25:47 +0530

    READONLY & other small things - Objects

--- js/objects.js
+++ js/objects.js
@@ -19,3 +19,15 @@ function userMsg(user) {
     return `${user.name} is ${user.age} years old`;
 }
 userMsg({ name: "KP", age: 24 });
+let user1 = {
+    _id: "321654",
+    name: "Vab",
+    age: 21
+};
+console.log(user1);
+const card1 = {
+    cvv: 321,
+    cardnumber: "3214 9878 6545 3212",
+    expiry: "12/29"
+};
+console.log(card1);
--- ts/objects.ts
+++ ts/objects.ts
@@ -21,6 +21,7 @@ createTrip(testObj); // when passing object in this fashion ts will accept extra
 
 // creating custom types ( type alias )
 type User = {
+    readonly _id?: string;
     name: string;
     age: number;
 };
@@ -30,3 +31,33 @@ function userMsg(user: User): string{
 }
 
 userMsg({name: "KP", age: 24})
+
+
+let user1: User = {
+    _id: "321654",
+    name: "Vab",
+    age: 21
+} 
+
+// user1._id = "231"  // can't modify readonly property
+
+console.log(user1);
+
+
+
+type cardNumber = {
+    cardnumber: string;
+}
+type cardExpiry = {
+    expiry: string
+}
+type cardDetails = cardNumber & cardExpiry & {
+    cvv: number
+}
+
+const card1: cardDetails = {
+    cvv: 321,
+    cardnumber: "3214 9878 6545 3212",
+    expiry: "12/29"
+}
+console.log(card1);
